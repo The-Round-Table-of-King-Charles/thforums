03f02198936af9569de038f28c1017227b7078d0
mcadelacruz <mcadelacruz@tip.edu.ph>
2025-04-06 14:22:16 +0800

    added profile updates and profile pics

--- thforums/forms.py
+++ thforums/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from thforums.models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username:", validators=[DataRequired(), Length(min=2, max=20)])
@@ -25,3 +27,21 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password:", validators=[DataRequired()])
     remember = BooleanField("Remember Me?")
     submit = SubmitField("Login")
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField("Username:", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email:", validators=[DataRequired(), Email()])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username already taken!")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email already taken!")
--- thforums/routes.py
+++ thforums/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template, url_for, flash, redirect, request
+import os
+
+from flask import render_template, url_for, flash, redirect, request, current_app
 from flask_login import login_user, logout_user, current_user, login_required
 from thforums import app, db, bcrypt
+from PIL import Image
 
 # it has to be thforums.<module> because we are in a package under the thforums folder
-from thforums.forms import RegistrationForm, LoginForm # import from forms.py
+from thforums.forms import RegistrationForm, LoginForm, UpdateProfileForm
 from thforums.models import User, Post # import from models.py
 
 # mock database
@@ -25,6 +28,34 @@ posts = [
     },
 ]
 
+def save_picture(form_picture):
+    random_hex = os.urandom(8).hex()
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+
+    # open the image
+    i = Image.open(form_picture)
+
+    # crop the image to a square
+    width, height = i.size
+    min_dim = min(width, height)
+    left = (width - min_dim) // 2
+    top = (height - min_dim) // 2
+    right = left + min_dim
+    bottom = top + min_dim
+    i = i.crop((left, top, right, bottom))
+
+    # resize the cropped image to the desired size
+    output_size = (125, 125)
+    i = i.resize(output_size)
+
+    # save the image
+    i.save(picture_path)
+
+    # return the filename
+    return picture_fn
+
 # website routes
 @app.route("/")
 @app.route("/home")
@@ -38,7 +69,26 @@ def about():
 @app.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html", title="Profile", posts=posts)
+    return render_template("profile.html", title="Profile")  # Removed unused posts parameter
+
+@app.route("/profile/update", methods=["GET", "POST"])
+@login_required
+def update_profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your profile has been updated!", "success")
+        return redirect(url_for("profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template("update_profile.html", title="Update Profile", image_file=image_file, form=form)
 
 # authentication routes, might put this on a seperate file
 @app.route("/register", methods=["GET","POST"])
@@ -65,7 +115,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
-            flash(f"Logged in successfully!", "error")
+            flash(f"Logged in successfully!", "success")  # Changed flash category to "success"
             return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash(f"Wrong credentials", "error")
