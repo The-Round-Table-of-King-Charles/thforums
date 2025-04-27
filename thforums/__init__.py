22bc337ad1b45c9aa583871d089f51f178dceba2
mcadelacruz <mcadelacruz@tip.edu.ph>
2025-04-27 13:30:19 +0800

    undid migration and database changes, and animated logo hover

--- thforums/__init__.py
+++ thforums/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
-import os
 
 # initialize flask app
 app = Flask(__name__)
@@ -16,18 +14,6 @@ bcrypt = Bcrypt(app)  # bcrypt for password hashing
 login_manager = LoginManager(app)  # login manager for user authentication
 login_manager.login_view = "login"  # redirect to login page if not authenticated
 login_manager.login_message_category = "info"  # flash message category for login
-migrate = Migrate(app, db)
-
-# initialize database if there is no current one
-if not os.path.exists("database.db"):
-    with app.app_context():
-        db.create_all()
-
-# migrates data if there are changes to models.py
-if not os.path.exists("migrations"):
-    os.system("flask db init")
-    os.system("flask db migrate -m 'Initial migration'")
-    os.system("flask db upgrade")
 
 # import routes to register them with the app
 from thforums import routes
